182b1a41760588a18c461d698764e8738274186d
Jeff Reiner <theundeadghost@gmail.com>
2023-09-13 13:35:25 +0200

    Fix env

--- env.mjs
+++ env.mjs
@@ -4,19 +4,16 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     SANITY_API_READ_TOKEN: z.string().min(1),
-    SANITY_API_WRITE_TOKEN: z.string().min(1).optional(),
+    SANITY_API_WRITE_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_SANITY_PROJECT_ID: z.string().min(1),
     NEXT_PUBLIC_SANITY_DATASET: z.string().min(1),
-    NEXT_PUBLIC_SANITY_PROJECT_TITLE: z.string().min(1),
   },
   runtimeEnv: {
     SANITY_API_READ_TOKEN: process.env.SANITY_API_READ_TOKEN,
     SANITY_API_WRITE_TOKEN: process.env.SANITY_API_WRITE_TOKEN,
     NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    NEXT_PUBLIC_SANITY_PROJECT_TITLE:
-      process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE,
   },
 });

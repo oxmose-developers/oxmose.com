61586813a81406b5c32dbeeb1d7b758314b60395
Jeff Reiner <theundeadghost@gmail.com>
2023-09-13 13:28:58 +0200

    Import t3 env on next.config

--- env.mjs
+++ env.mjs
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
 
 export const env = createEnv({
   server: {
@@ -16,6 +16,7 @@ export const env = createEnv({
     SANITY_API_WRITE_TOKEN: process.env.SANITY_API_WRITE_TOKEN,
     NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    NEXT_PUBLIC_SANITY_PROJECT_TITLE: process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE,
+    NEXT_PUBLIC_SANITY_PROJECT_TITLE:
+      process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE,
   },
-});
+})

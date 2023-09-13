834c0e73e168e41bf10354389c71b4cb1b378240
Jeff Reiner <theundeadghost@gmail.com>
2023-09-13 13:17:30 +0200

    Add t3/env

---
+++ env.ts
@@ -0,0 +1,21 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    SANITY_API_READ_TOKEN: z.string().min(1),
+    SANITY_API_WRITE_TOKEN: z.string().min(1).optional(),
+  },
+  client: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: z.string().min(1),
+    NEXT_PUBLIC_SANITY_DATASET: z.string().min(1),
+    NEXT_PUBLIC_SANITY_PROJECT_TITLE: z.string().min(1),
+  },
+  runtimeEnv: {
+    SANITY_API_READ_TOKEN: process.env.SANITY_API_READ_TOKEN,
+    SANITY_API_WRITE_TOKEN: process.env.SANITY_API_WRITE_TOKEN,
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_PROJECT_TITLE: process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE,
+  },
+});

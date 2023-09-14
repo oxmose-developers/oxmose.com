8eba1454d847890bb7e2c8f6375a37b6dc2a20eb
Jeff Reiner <theundeadghost@gmail.com>
2023-09-14 10:31:41 +0200

    Tweaks to metadata helper

--- lib/utils.metadata.ts
+++ lib/utils.metadata.ts
@@ -28,34 +28,6 @@ export function defineMetadata({
     title: metaTitle,
     themeColor: "#000",
     description,
-    openGraph: imageUrl
-      ? {
-          images: [imageUrl],
-        }
-      : undefined,
+    openGraph: imageUrl ? { images: [imageUrl] } : undefined,
   } satisfies Metadata;
 }
-
-/*
-<link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/favicon/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicon/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicon/favicon-16x16.png"
-    />
-    <link rel="manifest" href="/favicon/site.webmanifest" />
-    <link rel="shortcut icon" href="/favicon/favicon.ico" />
-    <meta name="msapplication-TileColor" content="#000000" />
-    <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-    */

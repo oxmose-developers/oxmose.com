eba6b41f1c16943cb00c34e4cfcf9de5737b8629
smudgy-g <adamgriff86@gmail.com>
2023-11-15 17:13:58 +0100

    feat: list of artists

--- app/(site)/artists/page.tsx
+++ app/(site)/artists/page.tsx
@@ -1,3 +1,29 @@
-export default function ArtistPage() {
-  return <div>TODO</div>;
+import { getArtistList } from "lib/sanity.fetch";
+import { resolveHref } from "lib/sanity.links";
+import Link from "next/link";
+
+
+export default async function ArtistPage() {
+  const artists = await getArtistList();
+  return (
+    <div>
+      {
+        artists && artists.map((artist) => {
+          const href = resolveHref(artist._type, artist.slug);
+          if (!href) {
+            return null;
+          }
+          return (
+            <Link 
+              key={artist._id} 
+              href={href} 
+              className="border-b border-t py-3 px-5 w-full block text-5xl font-medium tracking-tight"
+            >
+              {artist.name}
+            </Link>
+          )
+        })
+      }
+    </div>
+  )
 }

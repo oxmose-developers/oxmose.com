5ab9b338912cfff901e8e67a67b6c4acbb18b44b
smudgy-g <adamgriff86@gmail.com>
2023-11-23 17:49:07 +0100

    style(releaseCard): inner heights  responsiveness

--- components/pages/release/ReleaseCard.tsx
+++ components/pages/release/ReleaseCard.tsx
@@ -5,7 +5,7 @@ export default function ReleaseCard({data}) {
   const {release, href} = data;
   return (
     <div className="relative text-slate-800 border border-slate-600 mt-[-1px] ml-[-1px]">
-      <div className="flex flex-col md:grid md:grid-rows-[2fr_5fr] md:grid-cols-[1fr_3fr] md:gap-2 md:py-6 md:px-7">
+      <div className="flex flex-col md:grid md:grid-rows-[2fr_5fr] md:grid-cols-[1fr_3fr] max-h-[465px] md:gap-2 md:py-6 md:px-7 h-[435px] lg:h-auto">
 
         
         <h2 className="px-7 pt-5 text-2xl font-semibold tracking-tight md:order-first md:col-span-2 md:text-4xl md:p-0 lg:text-5xl hover:text-slate-600">
@@ -31,8 +31,6 @@ export default function ReleaseCard({data}) {
             {release.artist.name}
           </Link>
         </h3>
-
-
       </div>
       <div className="static bottom-0 w-full flex justify-between border-t border-slate-600 px-7 py-2 uppercase text-md font-semibold">
         <Link href={href} className="block">more</Link>

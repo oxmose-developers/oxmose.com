00660892b06c66bbe5c1fda0406ccad616bdc7fa
smudgy-g <adamgriff86@gmail.com>
2023-12-07 14:42:43 +0100

    style: text color

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -21,7 +21,7 @@ export function Header(props: HeaderProps) {
 
       {/* Description */}
       {description && (
-        <div className="mt-4 text-xl text-gray-600 md:text-2xl">
+        <div className="mt-4 text-xl text-slate-800 md:text-2xl">
           <CustomPortableText value={description} />
         </div>
       )}

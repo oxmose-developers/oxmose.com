88f9c95cd935319a62d86df20c2d44eec45b70da
smudgy-g <adamgriff86@gmail.com>
2023-12-07 14:41:44 +0100

    chore(List Item): add classes wrapper prop

--- components/pages/faq/ListItem.tsx
+++ components/pages/faq/ListItem.tsx
@@ -1,6 +1,6 @@
-export default function ListItem ({ content, handleClick }: { content: string, handleClick: () => void }) {
+export default function ListItem ({ content, handleClick, classesWrapper }: { content: string, classesWrapper: string, handleClick: () => void }) {
   return (
-    <div className="border-b border-slate-400 px-5 py-3" onClick={handleClick}>
+    <div className={`border-b border-slate-400 px-5 py-3 ${classesWrapper}`} onClick={handleClick}>
       <h2 
         className="block text-5xl font-medium tracking-tight"
       >

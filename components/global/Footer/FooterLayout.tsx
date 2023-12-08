56b9e982ec4bb9372011b1e5138be0d8bf0006fa
smudgy-g <adamgriff86@gmail.com>
2023-12-08 16:59:09 +0100

    style(footer): initial styling

--- components/global/Footer/FooterLayout.tsx
+++ components/global/Footer/FooterLayout.tsx
@@ -1,4 +1,5 @@
 import { CustomPortableText } from "components/shared/CustomPortableText";
+import Link from "next/link";
 import type { PortableTextBlock } from "sanity";
 import type { SettingsPayload } from "types";
 
@@ -11,13 +12,34 @@ export default function Footer(props: FooterProps) {
   const footer = data?.footer || ([] as PortableTextBlock[]);
 
   return (
-    <footer className="bottom-0 w-full bg-white py-12 text-center md:py-20">
-      {footer && (
+    <footer className="bottom-0 w-full bg-black text-white py-1 px-7 flex justify-between ">
+      {/* {footer && (
         <CustomPortableText
           paragraphClasses="text-md md:text-xl"
           value={footer}
         />
-      )}
+      )} */}
+      <ul className="uppercase flex gap-10">
+        <li>
+          <Link href="/publishing">Publishing</Link>
+        </li>
+        <li>          
+          <Link href="/follow">Follow</Link>
+        </li>
+        <li>
+          <Link href="/newsletter">Newsletter</Link>
+        </li>
+        <li>          
+          <Link href="/terms">Terms</Link>
+        </li>
+        <li>
+          <Link href="/privacy">Privacy</Link>
+        </li>
+        <li>          
+          <Link href="/faq">FAQ</Link>
+        </li>
+      </ul>
+      <p className="uppercase">&copy; 2023 oxmose</p>
     </footer>
   );
 }

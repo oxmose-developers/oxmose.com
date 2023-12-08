ffd243b785027a9d6f60912de82e5db96b5538b4
smudgy-g <adamgriff86@gmail.com>
2023-12-08 16:24:04 +0100

    style(release card): layout fix

--- components/pages/release/ReleaseCard.tsx
+++ components/pages/release/ReleaseCard.tsx
@@ -13,27 +13,30 @@ interface ReleaseCardProps {
 export default function ReleaseCard({ data }: { data: ReleaseCardProps }) {
   const {release, hrefRelease, hrefArtist} = data || {};
   return (
-    <div className="text-slate-800 border border-slate-400 mt-[-1px] ml-[-1px] flex flex-col md:grid md:grid-rows-[2fr_5fr_auto] md:grid-cols-[2fr_5fr]">
-      <Link href={hrefRelease} className="md:mt-auto md:pb-5 md:row-start-2">
-        <h2 className="px-7 pt-5 text-2xl font-semibold tracking-tight lg:text-3xl hover:text-slate-600 block">
-          {release?.title}
-        </h2>
-      </Link>
+    <div className="text-slate-800 border border-slate-400 mt-[-1px] ml-[-1px] flex flex-col">
+      <div className="flex flex-col md:flex-row md:items-end md:justify-between md:grow md:pr-7 md:pb-5">
+        <Link href={hrefRelease} className="flex">
+          <h2 className="px-7 pt-5 text-2xl font-semibold tracking-tight lg:text-3xl hover:text-slate-600 block md:basis-[40%] lg:basis-1/3">
+            {release?.title}
+          </h2>
+        </Link>
 
-      <Link href={hrefRelease} className="order-first md:row-start-2 md:col-start-2 mt-auto md:px-7 md:pb-5 hover:opacity-90">
-          <ImageBox
-          image={release?.coverImage}
-          imagePlaceholder={release?.coverImage?.lqip}
-          alt={`Cover image for ${release?.title}`}
-          height={500}
-          width={500}
-          classesWrapper="object-fit"
-        />
-      </Link>
+        <Link href={hrefRelease} className="order-first md:order-2 mt-auto hover:opacity-90">
+            <ImageBox
+            image={release?.coverImage}
+            imagePlaceholder={release?.coverImage?.lqip}
+            alt={`Cover image for ${release?.title}`}
+            height={500}
+            width={500}
+            classesWrapper="object-contain aspect-square mt-auto" 
+          />
+        </Link>
+
+      </div>
       
       {/* TODO - get artist slug for link */}
-      <Link href={hrefArtist} className="md:order-first md:col-span-2">
-        <h3 className="col-start-1 px-7 pb-5 text-2xl mt-auto tracking-tight md:font-semibold md:text-4xl md:py-5 lg:text-5xl hover:text-slate-600">
+      <Link href={hrefArtist} className="md:order-first flex">
+        <h3 className="px-7 pb-5 text-2xl mt-auto tracking-tight md:font-semibold md:text-4xl md:py-5 lg:text-5xl hover:text-slate-600 md:basis-1/3 md:mb-8">
             {release?.artist.name}
         </h3>
       </Link>
@@ -41,7 +44,7 @@ export default function ReleaseCard({ data }: { data: ReleaseCardProps }) {
       <ActionPanel 
         left={{href: hrefRelease, title: 'more'}}
         right={{href: '#', title: 'listen'}}
-        classesWrapper="row-start-3 col-span-2 border-t"
+        classesWrapper="border-t order-last max-h-fit"
       />
     </div>
   )

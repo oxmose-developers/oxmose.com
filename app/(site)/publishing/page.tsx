a1e56c29974ebcbe47a55b4b37b3f90915aa8d40
Jeff Reiner <theundeadghost@gmail.com>
2024-05-18 12:50:32 +0200

    Mobile design for Publishing page

--- app/(site)/publishing/page.tsx
+++ app/(site)/publishing/page.tsx
@@ -22,33 +22,43 @@ export default async function Page() {
     <>
       <h1 hidden>{`${page.title} | Oxmose`}</h1>
 
-      <section className="flex flex-col justify-center border-b border-white bg-black p-9 text-white lg:min-h-[60rem] lg:p-10 lg:py-16">
-        <h2 className="mb-52 text-[170px]/[85px]">
+      <section className="flex min-h-[25rem] flex-col border-b border-white bg-black px-9 py-5 text-white lg:min-h-[60rem] lg:justify-center lg:p-10 lg:py-16">
+        <h2 className="mb-2.5 text-[30px]/[1.375] lg:mb-52 lg:text-[170px]/[1.2]">
           {page.creativeServicesSection.title}
         </h2>
 
-        <div className="text-[60px]/[65px]">
+        <div className="text-[18px]/[20px] lg:text-[60px]/[65px]">
           <PortableText value={page.creativeServicesSection.content} />
         </div>
       </section>
 
-      <section className="flex flex-col justify-center border-b border-white bg-black p-9 text-white lg:min-h-[60rem] lg:p-10 lg:py-16">
-        <h2 className="mb-52 text-[170px]/[85px]">{page.scoreSection.title}</h2>
+      <section className="flex min-h-[25rem] flex-col border-b border-white bg-black px-9 py-5 text-white lg:min-h-[60rem] lg:justify-center lg:p-10 lg:py-16">
+        <h2 className="mb-2.5 text-[30px]/[1.375] lg:mb-52 lg:text-[170px]/[1.2]">
+          {page.scoreSection.title}
+        </h2>
 
-        <div className="text-[60px]/[65px]">
+        <div className="text-[18px]/[20px] lg:text-[60px]/[65px]">
           <PortableText value={page.scoreSection.content} />
         </div>
+
+        <p className="mt-auto text-right text-[30px]/[1.375] lg:mt-0 lg:text-left lg:text-[60px]/[65px]">
+          <a href={page.scoreSection.link.href}>
+            {page.scoreSection.link.name}
+          </a>
+        </p>
       </section>
 
-      <section className="flex flex-col justify-center bg-black p-9 text-white lg:min-h-[60rem] lg:p-10 lg:py-16">
-        <h2 className="mb-52 text-[170px]/[85px]">{page.syncSection.title}</h2>
+      <section className="flex min-h-[16rem] flex-col bg-black px-9 py-5 text-white lg:min-h-[60rem] lg:justify-center lg:p-10 lg:py-16">
+        <h2 className="mb-2.5 text-[30px]/[1.375] lg:mb-52 lg:text-[170px]/[1.2]">
+          {page.syncSection.title}
+        </h2>
 
-        <div className="text-[60px]/[65px]">
+        <div className="text-[18px]/[20px] lg:text-[60px]/[65px]">
           <PortableText value={page.syncSection.content} />
         </div>
       </section>
 
-      <section className="relative bg-white lg:min-h-[60rem]">
+      <section className="relative min-h-[12rem] bg-white lg:min-h-[60rem]">
         <Image
           alt=""
           fill
@@ -60,25 +70,19 @@ export default async function Page() {
         />
       </section>
 
-      <section className="border-b border-white bg-black text-white">
-        <div className="mb-28 px-9">
-          <h2 className="text-[170px]/[1.375]">Artists</h2>
+      <section className="border-b border-t border-white bg-black text-white lg:border-t-0">
+        <div className="px-9 pb-5 pt-4 lg:mb-28 lg:py-0">
+          <h2 className="text-[30px]/[1.375] lg:text-[170px]/[1.375]">
+            Artists
+          </h2>
         </div>
 
         <ul className="divide-y divide-white border-t">
+          {/* TODO PIPE UP */}
           <li>
             <Link
               href="#!"
-              className="block px-9 text-[80px]/[96px] hover:bg-white hover:text-black"
-            >
-              {"Nahal Kavand"}
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              href="#!"
-              className="block px-9 text-[80px]/[96px] hover:bg-white hover:text-black"
+              className="block px-9 pt-px text-[30px]/[40px] hover:bg-white hover:text-black lg:py-0 lg:text-[80px]/[96px]"
             >
               {"Nahal Kavand"}
             </Link>
@@ -87,7 +91,7 @@ export default async function Page() {
           <li>
             <a
               href={`mailto:${publishingLicenseRequestEmail}`}
-              className="block px-9 text-[170px]/[1.375] hover:bg-white hover:text-black"
+              className="block px-9 py-4 text-[30px]/[40px] hover:bg-white hover:text-black lg:py-0 lg:text-[170px]/[1.375]"
             >
               License request +
             </a>

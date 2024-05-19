845bf35d53b90eb4a79e20f3cca30ea7eb81ca60
Jeff Reiner <theundeadghost@gmail.com>
2024-05-19 14:19:21 +0200

    Finalize hooking up revalidate

--- app/(admin)/api/revalidate/route.ts
+++ app/(admin)/api/revalidate/route.ts
@@ -2,6 +2,8 @@ import { revalidateTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
 
+import { NEXT_TAGS } from "../../../../constants/tags";
+
 export async function POST(req: NextRequest) {
   try {
     const { body, isValidSignature } = await parseBody<{
@@ -17,9 +19,62 @@ export async function POST(req: NextRequest) {
       return new Response("Bad Request", { status: 400 });
     }
 
-    console.log(JSON.stringify(body, null, 2));
+    switch (body._type) {
+      case "artist": {
+        revalidateTag(NEXT_TAGS.ARTISTS);
+        revalidateTag(NEXT_TAGS.ARTISTS_STATIC_PARAMS);
 
-    revalidateTag(body._type);
+        if (body.slug) {
+          revalidateTag(body.slug);
+        }
+      }
+      case "faqs": {
+        revalidateTag(NEXT_TAGS.FAQ);
+
+        if (body.slug) {
+          revalidateTag(body.slug);
+        }
+      }
+      case "publishing": {
+        revalidateTag(NEXT_TAGS.PUBLISHING);
+      }
+      case "publishingArtist": {
+        revalidateTag(NEXT_TAGS.PUBLISHING);
+        revalidateTag(NEXT_TAGS.PUBLISHING_ARTISTS);
+        revalidateTag(NEXT_TAGS.PUBLISHING_ARTISTS_STATIC_PARAMS);
+
+        if (body.slug) {
+          revalidateTag(body.slug);
+        }
+      }
+      case "about": {
+        revalidateTag(NEXT_TAGS.ABOUT);
+      }
+      case "contact": {
+        revalidateTag(NEXT_TAGS.CONTACT);
+      }
+      case "legal": {
+        revalidateTag(NEXT_TAGS.LEGAL_STATIC_PARAMS);
+
+        if (body.slug) {
+          revalidateTag(body.slug);
+        }
+      }
+      case "release": {
+        revalidateTag(NEXT_TAGS.RELEASES);
+
+        if (body.slug) {
+          revalidateTag(body.slug);
+        }
+      }
+      default: {
+        revalidateTag(body._type);
+
+        if (body.slug) {
+          revalidateTag(body.slug);
+        }
+      }
+    }
 
     return NextResponse.json({
       status: 200,
--- constants/tags.ts
+++ constants/tags.ts
@@ -1,12 +1,17 @@
 export const NEXT_TAGS = {
   FAQ: "faq",
+
   PUBLISHING: "publishing",
   PUBLISHING_ARTISTS: "publishingArtists",
   PUBLISHING_ARTISTS_STATIC_PARAMS: "publishingArtistsStaticParams",
+
   RELEASES: "releases",
+
   LEGAL_STATIC_PARAMS: "legalStaticParams",
+
   ABOUT: "about",
+  CONTACT: "contact",
+
   ARTISTS: "artists",
   ARTISTS_STATIC_PARAMS: "artistsStaticParams",
-  CONTACT: "contact",
 } as const;

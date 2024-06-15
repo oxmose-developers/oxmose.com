379b5a39aa75552e1b56f3751fb43e468b5a3981
Jeff Reiner <theundeadghost@gmail.com>
2024-06-15 17:03:08 +0200

    Force Static

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -60,6 +60,8 @@ const monumentGrotesk = localFont({
   variable: "--font-monument-grotesk",
 });
 
+export const dynamic = "force-static";
+
 export default function RootLayout({
   children,
 }: {

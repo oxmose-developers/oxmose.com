6f1b1a4b8b453f8ad508ee439f07fd005300d76a
Jeff Reiner <theundeadghost@gmail.com>
2024-07-13 19:08:05 +0200

    Stub in API

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,16 +1,39 @@
 "use server";
 
+const listId = "";
+
+const apiKey = "";
+
 export async function subscribeToNewsletter(
   prevState: { message: string; success: boolean },
   formData: FormData,
 ) {
   "use server";
 
-  const rawFormData = {
-    email: formData.get("email"),
-  };
+  const response = await fetch(
+    `https://emailoctopus.com/api/1.6/lists/${listId}/contacts`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        api_key: apiKey,
+        email_address: formData.get("email"),
+        /**
+         * @note Needed for double-opt in
+         */
+        status: "PENDING",
+      }),
+    },
+  );
 
-  console.log(rawFormData);
+  if (!response.ok) {
+    const body = await response.json();
+
+    return {
+      code: body.code,
+      message: body.message,
+      success: false,
+    };
+  }
 
   return {
     message: "Success",

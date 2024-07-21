985dfb17978f9ccf97450e9898053eeb2dbb5858
Jeff Reiner <theundeadghost@gmail.com>
2024-07-21 16:34:00 +0200

    Add webm video

--- app/(site)/publishing/components/PublishingVideo.tsx
+++ app/(site)/publishing/components/PublishingVideo.tsx
@@ -2,12 +2,14 @@ export default function PublishingVideo() {
   return (
     <video
       className="absolute inset-0 h-full w-full object-cover"
-      src="/publishing-video.mp4"
       controls={false}
       autoPlay
       playsInline
       muted
       loop
-    />
+    >
+      <source src={`/publishing-video.webm`} type="video/webm" />
+      <source src={`/publishing-video.mp4`} type="video/mp4" />
+    </video>
   );
 }

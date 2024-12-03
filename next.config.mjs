eb1c71c08b6f26cf6fcadb98844e40384c081cb1
Jeff Reiner <theundeadghost@gmail.com>
2024-12-03 20:09:11 +0100

    Delete redirect page and put it in the config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const config = {
+  async redirects() {
+    return [
+      {
+        source: "/faq",
+        destination: "/faq/orders",
+        permanent: false,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { hostname: "cdn.sanity.io" },

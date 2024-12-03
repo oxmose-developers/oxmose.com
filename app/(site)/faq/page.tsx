8ec5f728fee3e93fc7e84069db148d7708d32e15
Jeff Reiner <theundeadghost@gmail.com>
2024-12-03 19:15:28 +0100

    hardcode faq redirect

--- app/(site)/faq/page.tsx
+++ app/(site)/faq/page.tsx
@@ -1,11 +1,5 @@
 import { redirect } from "next/navigation";
 
-import { fetchFaqs } from "../../../lib/sanity/queries";
-
-export default async function Page() {
-  const faqs = await fetchFaqs();
-
-  const firstFaq = faqs[0];
-
-  return redirect(`/faq/${firstFaq.slug.current}`);
+export default function Page() {
+  return redirect(`/faq/orders`);
 }

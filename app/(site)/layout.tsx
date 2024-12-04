679ddcd8a9a83ac4a88b12f2b0ebea13ed6bd4f3
Jeff Reiner <theundeadghost@gmail.com>
2024-12-04 14:23:02 +0100

    Fix body height on older browsers perhaps not supporing 100svg

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -108,7 +108,7 @@ export default function RootLayout({
       className={`${monumentGrotesk.variable} antialias`}
       suppressHydrationWarning
     >
-      <body className="flex min-h-[calc(100svh+4.5rem)] flex-col">
+      <body className="flex flex-col">
         <Providers>
           <Navigation />
 

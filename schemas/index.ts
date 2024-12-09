3a3506c76d96d3099b5e8c5753014f20c5fc5243
Jeff Reiner <theundeadghost@gmail.com>
2024-12-09 20:07:19 +0100

    Rename

--- schemas/index.ts
+++ schemas/index.ts
@@ -13,7 +13,7 @@ import releaseDate from "./objects/releaseDate";
 import track from "./objects/track";
 import trackList from "./objects/trackList";
 import twoColumn from "./objects/twoColumn";
-import publishingArtist from "./publishingArtist";
+import publishingArtist from "./publishing-artist";
 import release from "./release";
 import about from "./singletons/about";
 import contact from "./singletons/contact";

f273813290f60f6439b5700d12b12098747675cf
Jeff Reiner <theundeadghost@gmail.com>
2024-12-16 18:01:07 +0100

    Adding in redirects for old site

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -3,6 +3,56 @@ const config = {
   async redirects() {
     return [
       {
+        source: "/home",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/terms",
+        destination: "/terms-and-conditions",
+        permanent: true,
+      },
+      {
+        source: "/catalogue",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/visuals",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/videos",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/staff",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/newsletter",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/artist/shcaa",
+        destination: "/artists/sacha-khalife",
+        permanent: true,
+      },
+      {
+        source: "/artist/roberto-musci-",
+        destination: "/artists/roberto-musci",
+        permanent: true,
+      },
+      {
+        source: "/artist/:slug",
+        destination: "/artists/:slug",
+        permanent: true,
+      },
+      {
         source: "/faq",
         destination: "/faq/orders",
         permanent: false,

189e55ece9357fac200bc0229a5553305d4b3c6c
Jeff Reiner <theundeadghost@gmail.com>
2025-01-09 20:22:58 +0100

    Fix

--- site/app/components/client-only.tsx
+++ site/app/components/client-only.tsx
@@ -6,5 +6,4 @@ import type { FunctionComponent, PropsWithChildren } from "react";
 const ClientOnly: FunctionComponent<PropsWithChildren> = ({ children }) =>
   children;
 
-// @ts-expect-error - This is a valid JSX element
 export default dynamic(() => Promise.resolve(ClientOnly), { ssr: false });

0e99609339e3580077441eafb1177566901dd977
Jeff Reiner <theundeadghost@gmail.com>
2025-01-10 20:40:38 +0100

    Hush

--- site/app/components/client-only.tsx
+++ site/app/components/client-only.tsx
@@ -6,4 +6,5 @@ import type { FunctionComponent, PropsWithChildren } from "react";
 const ClientOnly: FunctionComponent<PropsWithChildren> = ({ children }) =>
   children;
 
+// @ts-expect-error - Ignore Error, occurs in Vercel only it seems
 export default dynamic(() => Promise.resolve(ClientOnly), { ssr: false });

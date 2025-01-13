1d3a9a6ae459538090092c97cdcec0bfda5ae9ac
Jeff Reiner <theundeadghost@gmail.com>
2025-01-13 14:30:21 +0100

    Try to fix about page first paragraph size

--- site/app/about/page.tsx
+++ site/app/about/page.tsx
@@ -38,7 +38,7 @@ export default async function Page() {
       <h1 hidden>{`${page.title} | Oxmose`}</h1>
 
       <section className="about-page-section flex items-center border-b border-black p-9 md:p-10">
-        <div className="text-oxe-md md:text-oxe-xxxxl/20.5">
+        <div className="text-oxe-md md:text-[4.75rem]/20.5">
           <PortableText value={page.part1} />
         </div>
       </section>
@@ -58,7 +58,7 @@ export default async function Page() {
       </section>
 
       <section className="about-page-section flex items-center border-b border-black p-9 md:p-10">
-        <div className="text-oxe-md md:text-oxe-xxxxl/20.5">
+        <div className="text-oxe-md md:text-[4.75rem]/20.5">
           <PortableText value={page.part3} />
         </div>
       </section>
@@ -78,7 +78,7 @@ export default async function Page() {
       </section>
 
       <section className="about-page-section flex items-center border-b border-black p-9 md:p-10">
-        <div className="text-oxe-md md:text-oxe-xxxxl/20.5">
+        <div className="text-oxe-md md:text-[4.75rem]/20.5">
           <PortableText value={page.part5} />
         </div>
       </section>

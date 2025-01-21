876f1d536fb3e283883ee0e9e05c40c718157f75
Oxmose Dev <office@oxmose.com>
2025-01-21 14:24:59 +0100

    Linking from the about section

--- site/app/components/about-credit-article.tsx
+++ site/app/components/about-credit-article.tsx
@@ -1,10 +1,9 @@
+import { Fragment } from "react";
 import { hasAtLeast } from "remeda";
 
 import type { Credits } from "../../lib/sanity";
 
 export default function CreditArticle({ credit }: { credit: Credits }) {
-  const joinHtmlString = `<span class="hidden md:inline">, </span><br class="md:hidden" />`;
-
   return (
     <article>
       <h3 className="text-oxe-xxs font-medium uppercase md:text-oxe-md">
@@ -16,23 +15,46 @@ export default function CreditArticle({ credit }: { credit: Credits }) {
           {credit.namedItems.map((namedItem) => (
             <p className="text-oxe-xxs md:text-oxe-md" key={namedItem._key}>
               <span className="uppercase">{namedItem.name}</span>{" "}
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: namedItem.items
-                    .map((item) => item.name)
-                    .join(joinHtmlString),
-                }}
-              ></span>
+              {namedItem.items.map((item, idx) => (
+                <Fragment key={`${idx}-${item.name}`}>
+                  {!!item?.link ? (
+                    <a href={item.link} target="_blank">
+                      {item.name}
+                    </a>
+                  ) : (
+                    <span>{item.name}</span>
+                  )}
+                  {idx !== namedItem.items.length - 1 && (
+                    <>
+                      <span className="hidden md:inline">, </span>
+                      <br className="md:hidden" />
+                    </>
+                  )}
+                </Fragment>
+              ))}
             </p>
           ))}
         </>
       ) : (
-        <p
-          className="text-oxe-xxs md:text-oxe-md"
-          dangerouslySetInnerHTML={{
-            __html: credit.items.map((item) => item.name).join(joinHtmlString),
-          }}
-        ></p>
+        <p className="text-oxe-xxs md:text-oxe-md">
+          {credit.items.map((item, idx) => (
+            <Fragment key={`${idx}-${item.name}`}>
+              {!!item?.link ? (
+                <a href={item.link} target="_blank">
+                  {item.name}
+                </a>
+              ) : (
+                <span>{item.name}</span>
+              )}
+              {idx !== credit.items.length - 1 && (
+                <>
+                  <span className="hidden md:inline">, </span>
+                  <br className="md:hidden" />
+                </>
+              )}
+            </Fragment>
+          ))}
+        </p>
       )}
     </article>
   );

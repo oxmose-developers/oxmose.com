c52fce7b7a634760e9a317f700038497391e154a
Oxmose Dev <office@oxmose.com>
2025-01-24 14:11:25 +0100

    Just ignore this file

--- site/app/components/logo-animation.tsx
+++ site/app/components/logo-animation.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 "use client";
 
 import "@aarsteinmedia/dotlottie-player-light";
@@ -46,7 +48,6 @@ export default function LogoAnimation() {
   }, [pathname]);
 
   return (
-    /* @ts-expect-error Ignore: Type error: Property 'dotlottie-player' does not exist on type 'JSX.IntrinsicElements' */
     <dotlottie-player
       ref={animation}
       src="/V04_front_page_v01_192.lottie"

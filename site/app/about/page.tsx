74934eab9dc4b22a3070d04db54907f3bf123f52
Oxmose Dev <office@oxmose.com>
2025-02-05 11:28:02 +0100

    Tweaks to fix about page team photo

--- site/app/about/page.tsx
+++ site/app/about/page.tsx
@@ -86,7 +86,7 @@ export default async function Page() {
       <section className="relative">
         <Image
           alt="Team photo"
-          className="max-h-[calc(100vh-60px)] object-cover object-bottom"
+          className="max-h-[calc(100vh-var(--footer-height))] object-cover object-bottom xl:object-center"
           loading="lazy"
           priority={false}
           sizes="100vw"
